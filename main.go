1dce650bdc74bcdf15ff91cbe1aedcfef55bfb31
Zehua Lu <analysek4@gmail.com>
2018-09-26 21:30:33 +0800

    implement StringService interface

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 // StringService provides operations on strings.
 package main
 
+import (
+	"errors"
+	"strings"
+)
+
 type StringService interface {
 	Uppercase(string) (string, error)
 	Count(string) int
 }
+
+type stringservice struct{}
+
+func (stringservice) Uppercase(s string) (string, error) {
+	if s == "" {
+		return "", ErrEmpty
+	}
+	return strings.ToUpper(s), nil
+}
+
+func (stringservice) Count(s string) int {
+	return len(s)
+}
+
+// ErrEmpty is returned when input string is empty
+var ErrEmpty = errors.New("Empty string")
